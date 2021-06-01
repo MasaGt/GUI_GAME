6c901763f7088a1f41ea85ec133d5543da8e0665
MasaGt <MasaGt@users.noreply.github.com>
2021-06-01 14:16:53 +1200

    fix the layout of Telephone

--- src/game/views/TelephonePanel.java
+++ src/game/views/TelephonePanel.java
@@ -8,33 +8,45 @@ package game.views;
 import game.controller.GameController;
 import game.util.Const;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.util.Observable;
 import java.util.Observer;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
- *
+ * Panel for Telephone.
  * @author Masaomi
  */
-public class TelephonePanel extends Panel implements Observer{
+public class TelephonePanel extends Panel implements Observer {
 
+    //components
     private JLabel label;
     private JButton closeBtn;
+    private JPanel buttonPanel;
     
+
+    //seize variables
+    private final int PANEL_WIDTH = 300;
+    private final int PANEL_HEIGHT = 100;
+
     public TelephonePanel() {
-        
+
         super(new BorderLayout());
-        
+        setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+
         label = new JLabel();
+        label.setHorizontalAlignment(JLabel.CENTER);
         this.add(label, BorderLayout.CENTER);
         
+        buttonPanel = new JPanel();
+        add(buttonPanel, BorderLayout.SOUTH);
         closeBtn = new JButton(Const.CLOSE_BUTTON);
-        this.add(closeBtn, BorderLayout.SOUTH);
-        
-        
+        buttonPanel.add(closeBtn);
+
     }
-    
+
     @Override
     public Object getParam() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -45,14 +57,24 @@ public class TelephonePanel extends Panel implements Observer{
         closeBtn.addActionListener(controller);
     }
 
+    /**
+     * receive a telephone text from Telephone.java.
+     * @param o
+     * @param arg telephone text.
+     */
     @Override
     public void update(Observable o, Object arg) {
         if (arg instanceof String) {
             render((String) arg);
         }
     }
-    
-    private void render (String telText) {
-        label.setText(telText);
+
+    /**
+     * render a telephone text.
+     * @param telText 
+     */
+    private void render(String telText) {
+        //enable lable to wrap text.
+        label.setText("<html>" + telText + "</html>");
     }
 }
